51231f223ce5820c0290b50de2f6c2c668d06bad
Fatihu AbdulGaniyu <abdulghaniyyfaatih2@gmail.com>
2026-09-04 17:44:09 +0100

    set-up the POST route

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,17 @@ import {
 const app: Express = express();
 const port = 3000;
 
-app.get("/", (request: Request, response: Response) => {
-  response
+app.use(express.json());
+
+app.post("/orders", (req: Request, res: Response) => {
+
+  const detail = req.body;
+  const order = createOrder(detail.customerName, detail.items);
+  res.status(201).json(order);
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res
     .status(200)
     .send(
       "Wanna test the order management system? Use the endpoints to create, update, and manage orders.",
